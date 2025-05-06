a1a79d4bd1556bd856209177cc1982ef170323bc
Hewida Ali <112735381+Hiida3li@users.noreply.github.com>
2025-05-06 14:16:31 +0400

    .

--- src/data_explorations/insights.py
+++ src/data_explorations/insights.py
@@ -1,6 +1,9 @@
 import os
 import pandas as pd
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import seaborn as sns
+import warnings
+
+warnings.filterwarnings('ignore')
 
